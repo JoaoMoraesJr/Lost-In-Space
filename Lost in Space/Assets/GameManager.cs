42eac2767d0819b155c2d48235127d097c73f6bb
JoaoMoraesJr <joao_junior1997@hotmail.com>
2019-06-27 09:13:42 -0300

    Scene Manager

---
+++ Lost in Space/Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.GetComponent<Health>().health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(0, LoadSceneMode.Additive);
+        }
+    }
+}
